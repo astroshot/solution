2ac886b5d03b2e1824e70463fc7ff6a58ebee352
lzh <astroshot@outlook.com>
2018-07-26 21:55:35 +0800

    add previous src

---
+++ src/link/ListNode.java
@@ -0,0 +1,13 @@
+package link;
+
+/**
+ * Created by Altair on 2017/04/20.
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+}
